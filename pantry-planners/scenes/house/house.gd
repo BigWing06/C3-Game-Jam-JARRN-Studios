4fca76562ea6f97068499e58f8c19382d335d75e
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2026-03-13 22:58:58 -0400

    Update to house and pantry scenes

--- pantry-planners/scenes/house/house.gd
+++ pantry-planners/scenes/house/house.gd
@@ -1,4 +1,26 @@
 extends Sprite2D
 
-func check_food_need() -> bool:
-	return true
+signal food_needed_changed
+
+var food_needed: int = 0
+
+# This function is called by the food pantries on all houses
+# in its radius to see if they need to take food
+func get_need(type: String):
+	return food_needed
+
+# This function is called by the food pantries when they
+# give food to the houses
+func give_food(type: String):
+	food_needed -= 1
+	food_needed_changed.emit()
+
+# This function is called by the food_timeout timer
+# when the amount of food needs to be increased on the house
+func add_food():
+	food_needed += 1
+	food_needed_changed.emit()
+
+
+func _on_food_needed_changed() -> void:
+	$food_amount_label.text = str(get_need(""))
--- pantry-planners/scenes/pantry/pantry.gd
+++ pantry-planners/scenes/pantry/pantry.gd
@@ -5,9 +5,13 @@ extends Sprite2D
 @export var active: bool = false 
 # Signal that is emmited when the active state changes
 signal active_changed()
+signal food_changed()
+
+var food: int #TODO Change this a list with different food types
 
 func _ready():
 	set_active(active)
+	set_food("", 10)
 
 # Used to set state of the active variable. Handles resetting, 
 # starting, and stopping the effect_timer
@@ -19,10 +23,44 @@ func set_active(new_state: bool):
 	else:
 		$effect_timer.stop()
 
+func set_food(type: String, amount: int):
+	food = amount
+	food_changed.emit()
+
+# This function adds one food of a specific type to the pantry
+func add_food(type: String):
+	set_food(type, get_food_amount(type) + 1)
+	
+# This function is called by houses in radius when
+# they want to take food from the pantry
+func take_food(type: String) -> void:
+	food -= 1
+	food_changed.emit()
+
 # This function is called by the effect timer and calls the check_food_need()
 # function in all houses that are in it's effect_radius
 func effect_houses() -> void:
-	for area in $effect_radius.get_overlapping_areas():
-		var house = area.get_parent()
-		if (house.is_in_group("house")):
-			house.check_food_need()
+	while (food > 0):
+		var greatest_need: int = 0
+		var neediest_house = null
+		print("Looping list")
+		for area in $effect_radius.get_overlapping_areas():
+			var house = area.get_parent()
+			if house.is_in_group("house"):
+				var house_need = house.get_need("null")
+				print(str(greatest_need) + "    " + str(house.get_need("null")))
+				if greatest_need < house_need: #TODO replace null with food type as string
+					greatest_need = house_need
+					neediest_house = house
+		print(greatest_need)
+		if greatest_need == 0:
+			break
+		neediest_house.give_food("null") #TODO replace null with food type as string
+		take_food("null")
+
+# This function is called to get the amount of a specific food type
+func get_food_amount(type: String) -> int:
+	return food
+
+func _on_food_changed() -> void:
+	$food_amount_label.text = str(get_food_amount("null"))
