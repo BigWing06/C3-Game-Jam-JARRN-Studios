cb924f7ff87d6a9a05ba210a5a5b305616727166
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2026-03-15 15:08:43 -0400

    Added Button Art

--- pantry-planners/scenes/Warehouse Display/display_scene.gd
+++ pantry-planners/scenes/Warehouse Display/display_scene.gd
@@ -2,10 +2,17 @@ extends HBoxContainer
 
 @export var texture: Image
 @export var type: String
+@export var normal_texture: CompressedTexture2D
+@export var pressed_texture: CompressedTexture2D
+@export var hover_texture: CompressedTexture2D
+@export var disabled_texture: CompressedTexture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Label.text = type
+	$Button.texture_hover  = hover_texture
+	$Button.texture_pressed  = pressed_texture
+	$Button.texture_normal  = normal_texture
+	$Button.texture_disabled  = disabled_texture
 	
 func update_text(num):
 	$Label.text = str(num)
