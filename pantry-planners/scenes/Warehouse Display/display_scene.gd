dec0f510fb4c89b4221b50bbad912c809bb2de86
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2026-03-15 14:42:54 -0400

    Added Art

--- pantry-planners/scenes/Warehouse Display/display_scene.gd
+++ pantry-planners/scenes/Warehouse Display/display_scene.gd
@@ -5,11 +5,10 @@ extends HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TextureRect.texture = texture
 	$Label.text = type
 	
 func update_text(num):
-	$Label.text = type + " " + str(num)
+	$Label.text = str(num)
 	
 func get_button():
 	return $Button
